0fb9645517eb708f6b31c9dfacc5a15c7206c656
dfucci <davfuc@gmail.com>
2015-09-01 20:29:14 +0300

    completed

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,19 @@
 (ns alphabet-cipher.coder)
 
+;Constants
+(def alphabet (String. "abcdefghijklmnopqrstuvwxyz"))
+(def alphabet-count (count alphabet))
+
+;Match a single letter to its encoded version by keyword
+(defn- encode-single [m k] 
+  (nth alphabet (mod (+ (.indexOf alphabet (str k)) (.indexOf alphabet (str m))) alphabet-count)))
+
+;and the other way around
+(defn- decode-single [m k]
+  (nth alphabet (mod  (+ (.indexOf alphabet (str m))  (- (count alphabet) (.indexOf alphabet (str k)))) alphabet-count)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map encode-single message (cycle keyword))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map decode-single message (cycle keyword))))
